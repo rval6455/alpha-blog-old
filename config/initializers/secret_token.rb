70b253120113c9ce3c15279758d68ac2e2802649
Randy Valentine <rval001@gmail.com>
2016-04-04 20:08:15 -0400

    Create rails application and setup home and about pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlphaBlog::Application.config.secret_token = '07650b856646039f355f71ddabe0754da8db3117ef8dda6398031523bc529a98b42ce656aa268d5dacc313bb78a8cd3e9d1cb7e5a944bd356b2a411ff033132a'
